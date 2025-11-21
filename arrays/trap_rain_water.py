a8e641c7eb428d4f24d070ecf6d592b1f7bb7b77
Ayush Joshi <ayush618officer@gmail.com>
2025-11-21 14:35:36 +0530

    trapping rain water

---
+++ arrays/trap_rain_water.py
@@ -0,0 +1,36 @@
+"""
+Find the maximum amount of water we can trap.
+
+i/p: [3, 0, 1, 2, 5]
+o/p: 6
+
+i/p: [3, 2, 1]
+o/p: 0
+"""
+
+def max_water(levels: list) -> int:
+    res = 0
+    length = len(levels)
+    
+    lmax = [None] * length
+    rmax = [None] * length
+
+    lmax[0] = levels[0]
+    for i in range(1, length):
+        lmax[i] = max(lmax[i - 1], levels[i])
+
+    rmax[length - 1] = levels[length - 1]
+    for i in range(length - 2, -1, -1):
+        rmax[i] = max(rmax[i + 1], levels[i])
+
+    for i in range(1, length):
+        res += min(lmax[i], rmax[i]) - levels[i]
+
+    return res
+
+
+if __name__ == '__main__':
+    print(max_water([3, 0, 1, 2, 5]))     # 6
+    print(max_water([7, 2, 1, 0, 5, 6]))  # 16
+    print(max_water([3, 2, 1]))           # 0
+    print(max_water([1, 2, 3]))           # 0
