b8800da0131d480d97af9613af83274d457c2f66
Morningstar618 <ayush618officer@gmail.com>
2025-12-06 13:55:37 +0530

    index-of-first-occurrence-sorted-array

---
+++ searching/ind_first_occ_sorted_arr.py
@@ -0,0 +1,39 @@
+"""
+Find the first occurrence of `k` in a sorted array.
+If the element is not found, return -1.
+
+Approach: Binary search
+
+Time -> O(logn), Space -> O(1)
+
+i/p: [1, 10, 10, 10, 20, 20, 40], k=20
+o/p: 4
+"""
+
+def find(nums: list, k: int) -> int:
+    """Find 1st occurrence of 'k'
+    in 'nums'"""
+    s = 0
+    e = len(nums) - 1
+    m = int((s + e) / 2)
+
+    while s <= e:
+        if nums[m] == k and nums[m - 1] != k or m == 0:
+            return m
+
+        if nums[m] >= k:
+            e = m - 1
+        else:
+            s = m + 1
+
+        m = int((s + e) / 2)
+
+    return -1
+
+
+if __name__ == '__main__':
+    nums1 = [1, 10, 10, 10, 20, 20, 40]
+
+    print(find(nums1, 20))      # 4
+    print(find(nums1, 50))      # -1
+    print(find([1, 1, 1], 1))   # 0
